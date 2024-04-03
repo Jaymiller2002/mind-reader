d4f7cf879f5719016768b548a9ea54412061333e
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-03 13:42:50 -0400

    added function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 // main.js
+const cards = document.querySelectorAll('.cards');
+
+function cards(){
+  let x = document.getElementById(".cards");
+  x.style.display = "block";
+}
+
 
 function init(){
   console.log("hello world!");
