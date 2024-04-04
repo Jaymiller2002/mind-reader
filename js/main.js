27eb5f385df9517a678521b620b5404316122372
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-04 10:07:23 -0400

    css n js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 // main.js
+function init(){
+  console.log("hello world!");
+}
+
+init()
+//setup function that calls to page and switches it with another
+
+
+//setup
+const page1 = document.querySelectorAll('.cards');
