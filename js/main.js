71ba6c7fdc21aa52e9387bd5b440291cd924b4c5
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-04 09:43:22 -0400

    restart

--- js/main.js
+++ js/main.js
@@ -1,14 +1,1 @@
 // main.js
-let currentPageIndex = 0;
-let cards = document.querySelectorAll('.card');
-let button = document.querySelectorAll('.button');
-
-
-function showPage() {
-  cards(currentPageIndex).classList.remove('visible');
-  currentPageIndex++;
-  if (currentPageIndex >= cards.length) {
-    currentPageIndex = 0;
-  }
-  cards(currentPageIndex).classList.add('visible');
-}
