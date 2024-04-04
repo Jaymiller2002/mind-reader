378e3d011f04ea83bc3f0028fcd76e57704c0da0
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-04 11:49:33 -0400

    js

--- js/main.js
+++ js/main.js
@@ -5,11 +5,13 @@ function init(){
 
 init()
 //setup function that calls to page and switches it with another
-
-
 //setup
 const button = document.querySelectorAll('.button');
 const cards = document.querySelectorAll('.cards');
 const content = document.querySelectorAll('.content');
-
+const div = document.querySelectorAll('.div')
+//Event Listeners
+button.addEventListener("click", ()=> {
+div.classList.toggle('div');
+})
 //setup for linking to other page id's
