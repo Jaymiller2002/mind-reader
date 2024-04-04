6348dc3830b236e374b630d82b612540a30dd920
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-04 11:50:23 -0400

    deleted

--- js/main.js
+++ js/main.js
@@ -9,9 +9,7 @@ init()
 const button = document.querySelectorAll('.button');
 const cards = document.querySelectorAll('.cards');
 const content = document.querySelectorAll('.content');
-const div = document.querySelectorAll('.div')
+
 //Event Listeners
-button.addEventListener("click", ()=> {
-div.classList.toggle('div');
-})
+
 //setup for linking to other page id's
