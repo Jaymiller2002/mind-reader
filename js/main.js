56859d2c7f2c74f6f202c63e0463594037087852
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-04 16:39:21 -0400

    js

--- js/main.js
+++ js/main.js
@@ -16,7 +16,17 @@ button.forEach(function(btn)) {
 btn.addEventListener("click", handleEvent);
 }
 //setup for linking to other page's
+let div = document.getElementById('#page1'); // Assumes element with id='button'
 
+button.onclick = function() {
+    let div = document.getElementById('#page2');
+    if (div.style.display !== 'none') {
+        div.style.display = 'block';
+    }
+    else {
+        div.style.display = 'none';
+    }
+};
 
 
 //Multiples of 9 function for 1-99
