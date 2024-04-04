238927b1b1d638f935efca23dac1377c7cc0d59a
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-03 22:16:48 -0400

    js update

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ let currentPageIndex = 0;
 let cards = document.querySelectorAll('.card');
 
 function showNextPage() {
-  cards[currentPageIndex].classList.remove('visible');
+  cards(currentPageIndex).classList.remove('visible');
   currentPageIndex++;
   if (currentPageIndex >= cards.length) {
     currentPageIndex = 0;
   }
-  cards[currentPageIndex].classList.add('visible');
+  cards(currentPageIndex).classList.add('visible');
 }
