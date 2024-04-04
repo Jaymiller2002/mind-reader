bc12a633c4e3fed0a40cf027cbc9ff1222f29435
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-04 14:58:39 -0400

    js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,8 @@ init()
 const button = document.querySelectorAll('.btn');
 const cards = document.querySelectorAll('.cards');
 const content = document.querySelectorAll('.content');
-
+const visible = document.querySelectorAll('.visible');
+const hidden = document.querySelectorAll('.hidden');
 //Event Listeners
 button.forEach(function(btn)) {
 btn.addEventListener("click", handleEvent);
