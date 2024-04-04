0d0024a445de5957a8ed464d29521ba807850484
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-03 21:50:19 -0400

    .

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
 function showPage(pageNumber) {
   // Hide all pages
   for (let i = 1; i <= 6; i++) {
-    let page = document.getElementById('page' + i);
+    let page = document.getElementById('page1' + i);
     if (page) {
       page.style.display = 'none';
     }
   }
   // Show the requested page
-  let currentPage = document.getElementById('page' + pageNumber);
+  let currentPage = document.getElementById('page2' + pageNumber);
   if (currentPage) {
     currentPage.style.display = 'block';
   }
