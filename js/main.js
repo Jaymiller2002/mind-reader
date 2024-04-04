c85527a7477e8385d99c23ecde802cfb7eea49fd
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-04 11:59:42 -0400

    changed js and html

--- js/main.js
+++ js/main.js
@@ -6,10 +6,13 @@ function init(){
 init()
 //setup function that calls to page and switches it with another
 //setup
-const button = document.querySelectorAll('.button');
+const button = document.querySelectorAll('.btn');
 const cards = document.querySelectorAll('.cards');
 const content = document.querySelectorAll('.content');
 
 //Event Listeners
+button.forEach(function(btn)){
+btn.addEventListener("click", handleEvent);
+}
 
 //setup for linking to other page id's
