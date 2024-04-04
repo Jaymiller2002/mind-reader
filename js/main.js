350a6f6526861ad33195f687834d07e5ab8cb4f1
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-04 13:21:58 -0400

    added link maybe

--- js/main.js
+++ js/main.js
@@ -9,13 +9,25 @@ init()
 const button = document.querySelectorAll('.btn');
 const cards = document.querySelectorAll('.cards');
 const content = document.querySelectorAll('.content');
-
+const pages = ['page1', 'page2']
 //Event Listeners
 button.forEach(function(btn)){
 btn.addEventListener("click", handleEvent);
 }
 
-cards.forEach(function(cards)){
-  cards.addEventListener("click", handleEvent);
-}
+
 //setup for linking to other page id's
+let next = document.getElementById('next'); // your DOM element
+
+next.addEventListener('click', function() {
+
+  var current = location.page1, // current page ie. MedTech.html
+      idx = pages.indexOf( current ); // find page in pages array
+
+  // go to next page if page exists
+  // otherwise go to first page   
+  if ( idx > -1 ) {
+    location.page1 = pages[ ++idx ] || pages[0];
+  }
+
+});
