63fda0e62caef82ba895778cb74a6b503c0ba102
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-04 21:21:16 -0700

    html,css,js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,43 @@
 // main.js
-function init(){
-  console.log("hello world!");
+let currentPageIndex = 1; // Start with page 1
+
+function showNextPage() {
+    switch (currentPageIndex) {
+        case 1:
+            currentPageIndex = 2;
+            break;
+        case 2:
+            currentPageIndex = 3;
+            break;
+        case 3:
+            currentPageIndex = 4;
+            break;
+        case 4:
+            currentPageIndex = 5;
+            break;
+        case 5:
+            currentPageIndex = 6;
+            break;
+        case 6:
+            currentPageIndex = 1; // Restart from page 1
+            break;
+        default:
+            currentPageIndex = 1;
+    }
+    showPage(currentPageIndex);
 }
 
-init()
-//setup function that calls to page and switches it with another
-//setup
-const button = document.querySelectorAll('.btn');
-const cards = document.querySelectorAll('.cards');
-const content = document.querySelectorAll('.content');
-
-
-//Event Listeners
-button.forEach(function(btn));{
-btn.addEventListener("click", handleEvent);
-}
-
-//setup for linking to other page's
-document.getElementById("page1").addEventListener("click", someFunction);
-
-
-//functions
 function showPage(pageNumber) {
-  // Hide all pages
-  const pages = document.querySelectorAll('.hidden');
-  pages.forEach(page => {
-      page.classList.add('hidden');
-  });
+    // Hide all pages
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => {
+        page.classList.add('hidden');
+    });
 
-  // Show the page with the specified number
-  const currentPage = document.getElementById(`page${pageNumber}`);
-  if (currentPage) {
-      currentPage.classList.remove('hidden');
-  }
+    // Show the page with the specified number
+    const currentPage = document.getElementById(`page${pageNumber}`);
+    if (currentPage) {
+        currentPage.classList.remove('hidden');
+    }
 }
 
